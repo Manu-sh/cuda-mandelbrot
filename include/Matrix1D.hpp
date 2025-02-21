b8ff3ad3b48de88bc4e803a6af4a705cf41e8dac
Manu-Sh <s3gmentationfault@gmail.com>
2025-02-21 07:22:30 +0100

    Matrix1D memory alignment 256 bit to use avx2

--- include/Matrix1D.hpp
+++ include/Matrix1D.hpp
@@ -1,8 +1,35 @@
 #pragma once
 #include <cstdint>
+#include <cstdlib>
 #include <stdexcept>
+#include <new>
 
-template <typename T>
+#if 0
+    // test to run with -O0
+    #include <iostream>
+    using std::cout, std::endl;
+    int main() {
+
+        struct __attribute__((__packed__)) big_t {
+            big_t() {
+                p = malloc(113);
+                cout << "constructor called, pointer " << p << '\n';
+            }
+            ~big_t() {
+                free(p);
+                cout << "called for pointer " << p << '\n';
+            }
+            void *p = nullptr;
+            char buf[12];
+        };
+
+        //cout << sizeof(big_t) << endl;
+        Matrix1D<big_t> mtx{3, 2};
+    }
+#endif
+
+// memory aligned to 256 bit (32 byte) to use avx2
+template <typename T, const size_t ALIGNMENT = 32>
 class Matrix1D {
 
     public:
@@ -14,7 +41,14 @@ class Matrix1D {
 
     public:
         explicit Matrix1D(uint16_t height, uint16_t width);
-        ~Matrix1D() { delete[] m_vct; }
+        ~Matrix1D() {
+            //delete[] m_vct;
+            if (!m_vct) return;
+            for (uint32_t i = 0; i < m_length; ++i)
+                m_vct[i].~T();
+
+            free(m_vct);
+        }
 
         T & at(uint16_t r, uint16_t c);
         inline T * unwrap() { return m_vct; }
@@ -46,15 +80,23 @@ class Matrix1D {
         uint16_t m_height;
 };
 
-template <typename T>
-Matrix1D<T>::Matrix1D(uint16_t height, uint16_t width)
+template <typename T, const size_t ALIGNMENT>
+Matrix1D<T, ALIGNMENT>::Matrix1D(uint16_t height, uint16_t width)
         : m_width{width}, m_height{height}, m_length{(uint32_t)width*height} {
 
-    m_vct = new T[m_length];
+    const auto bsize = sizeof(T) * m_length;
+    const auto aligned_bsize = ((bsize + ALIGNMENT - 1) / ALIGNMENT) * ALIGNMENT;
+    //printf("allocating block of %zu aligned to %zu\n", aligned_bsize, alignment);
+
+    m_vct = (decltype(m_vct)) aligned_alloc(ALIGNMENT, aligned_bsize);
+    new (m_vct) T[m_length]; // initialize the memory block calling in-place new[]
+
+    if (((long)m_vct) % ALIGNMENT != 0)
+        throw std::runtime_error("incorrectly aligned memory");
 }
 
-template <typename T>
-T & Matrix1D<T>::at(uint16_t r, uint16_t c) {
+template <typename T, const size_t ALIGNMENT>
+T & Matrix1D<T, ALIGNMENT>::at(uint16_t r, uint16_t c) {
 
     const uint32_t i = r * m_width + c; // row major order
     if (i >= m_length)
